7da4877ed5597e7f2dd489cc16090a954a1d6c72
«Vlad <funnytrofi@gmail.com>
2025-11-05 10:24:12 +0600

    fixed favorite

--- lib/features/news/presentation/pages/main_layout.dart
+++ lib/features/news/presentation/pages/main_layout.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:news_app/core/widgets/custom_bottom_nav_bar.dart';
+import 'package:news_app/features/news/presentation/bloc/favorites/favorites_bloc.dart';
+import 'package:news_app/features/news/presentation/bloc/favorites/favorites_event.dart';
 
 class MainLayout extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
@@ -23,6 +26,9 @@ class MainLayout extends StatelessWidget {
       bottomNavigationBar: CustomBottomNavBar(
         currentIndex: navigationShell.currentIndex,
         onTap: (index) {
+          if (index == 1) {
+            context.read<FavoritesBloc>().add(const LoadFavorites());
+          }
           navigationShell.goBranch(
             index,
             initialLocation: index == navigationShell.currentIndex,
